62ef84721321f105551cbbc2c6e5b303c5e27a37
Raparthi, Abhishek <araparthi@islander.tamucc.edu>
2024-09-11 23:30:14 -0500

    Added Component

--- View/view-app/src/App.js
+++ View/view-app/src/App.js
@@ -1,9 +1,10 @@
+import Note from "./Note";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Text Editor</h1>
-      <p>Lorem ipsum</p>
+    <div>
+      <Note/>
+      <Note/>
     </div>
   );
 }
---
+++ View/view-app/src/Note.js
@@ -0,0 +1,7 @@
+function Note(){
+    return(<div>
+        <h1>Title</h1>
+        <p>Body</p>
+    </div>);
+}
+export default Note;
